0ee7e94f9e5b5bd59fc04b4c4544be65fada5fe8
thebigbluebox <tong_901@hotmail.com>
2015-01-20 11:11:57 -0500

    updated resume

--- php/resumepage.php
+++ php/resumepage.php
@@ -24,7 +24,7 @@
 		<p><span class="title_small right">Programming Skills</span></p>
 		<ul>
 			<li class="sklist">Object Oriented:<b> Java/J2EE, Pivotal Labs Spring MVC, C#, C++, Python, XAML </b></li>
-			<li class="sklist">Web Related: <b>Full AMP stack, MEAN stack, CSS 3.0, HTML 5.0, UI/UX design experience</b></li>
+			<li class="sklist">Web Related: <b>Full AMP stack, MEAN stack, CSS 3.0, HTML 5.0, UI/UX design experience, Bootstrap</b></li>
 			<li class="sklist">Database: <b>MySQL, Microsoft SQL Server, SQLite, CouchDB, MongoDB</b></li>
 			<li class="sklist">Test and QA Experiences:<b> Selenium, JUnit</b></li>
 			<li class="sklist">Additional Experiences: <b>Android Development, Haskell, OpenGL,/ES OpenGL, LaTeX, Windows Store Apps, fluent in the art of Git and SVN</b></li>
